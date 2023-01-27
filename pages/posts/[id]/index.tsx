18757e7a0e1aabb02a0befeca4e028e79a1d7b4a
youspinmerond <djangoblnot@gmail.com>
2023-01-27 20:52:20 +0200

    add post component

--- pages/posts/[id]/index.tsx
+++ pages/posts/[id]/index.tsx
@@ -1,13 +1,38 @@
-fetch('http://localhost:3000/posts/1')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
+import styles from '../../../styles/post.module.sass'
+import Aside from 'components/Aside'
+import { useRouter } from 'next/router'
+import prisma from "@database"
+import { GetServerSideProps } from 'next'
 
-export default function Post() {
+export default function Post({postContent}:any) {
+  console.log(postContent)
   return (
-    <span>Post</span>
+    <div className={styles['main']}>
+      <div className={styles['post']}>
+        <h1 className={styles['title']}>{postContent.title}</h1>
+
+        <div className={styles['content']}>{postContent.body}</div>
+
+        <div className={styles["comments"]}>Comments</div>
+
+      </div>
+      <Aside/>
+    </div>
   )
+}
+
+
+
+
+
+
+export const getServerSideProps:GetServerSideProps = async (context) => {
+  const id = Number(context.query.id)
+  const res = await prisma.post.getPostById(id)
+  const result = JSON.parse(JSON.stringify(res))
+  return {
+    props: {
+      postContent: result//result
+    }
+  }
 }
