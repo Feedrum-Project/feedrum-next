2d668295126129190aa6caf48f014431f1d6ff19
youspinmerond <djangoblnot@gmail.com>
2023-08-31 19:27:16 +0200

    Revert "Revert "tags added.""
    This reverts commit 81dece200c5a70801a06220e4b3bee7ea0f3d8a2.

--- pages/createPost.tsx
+++ pages/createPost.tsx
@@ -1,9 +1,9 @@
 import styles from "../styles/create.module.sass";
 
 import { FormEvent, useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { IUser } from "types/User";
+import { useDispatch, useSelector } from "react-redux";
 import { HTMLtoMD, MDtoHTML } from "helpers/parsers.helper";
+import { z } from "zod";
 
 import Script from "next/script";
 import { Button } from "components/UI";
@@ -11,6 +11,7 @@ import Images from "module/CreateForm/Components/Images";
 import Editor from "module/CreateForm/Components/Editor";
 import View from "module/CreateForm/Components/View";
 import { useRouter } from "next/router";
+import { IStore } from "store/store";
 
 export default function CreatePost() {
   const [chapter, setChapter] = useState<"editor" | "view">("view");
@@ -19,7 +20,8 @@ export default function CreatePost() {
     content: ""
   });
   const [files, setFiles] = useState<File[]>();
-  const { user } = useSelector((state: { user: IUser }) => state);
+  const { user } = useSelector((state: IStore) => state);
+  const dispatch = useDispatch();
   const router = useRouter();
 
   useEffect(() => {
@@ -43,23 +45,49 @@ export default function CreatePost() {
     });
   }
 
-  function sub(e: FormEvent & { target: { title: HTMLInputElement } }) {
+  function sub(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     if (!e || !e.target) return;
-    const target = e.target;
+    const { target } = e as unknown as {
+      target: {
+        title: HTMLInputElement;
+        tags: HTMLInputElement;
+      };
+    };
 
     const { value: title } = target.title;
     const text: HTMLElement | null = document.getElementById("txt");
     if (!text) throw new Error("Couldn't be found field");
     const content = HTMLtoMD(MDtoHTML(text.innerHTML));
 
-    if (title.length < 8 || content.length < 100)
-      throw new Error("not enough symbols");
-
     const body = {
       body: { title, body: content },
-      tags: [{ name: "HTML" }, { name: "CSS" }, { name: "JavaScript" }]
+      tags: target.tags.value.split(" ").map((tag) => {
+        return { name: tag };
+      })
     };
+
+    const schema = z.object({
+      body: z.object({
+        title: z.string().min(8),
+        body: z.string().min(100)
+      }),
+      tags: z
+        .object({
+          name: z.string()
+        })
+        .array()
+    });
+    const isSucces = schema.safeParse(body).success;
+    if (!isSucces)
+      dispatch({
+        type: "addNotification",
+        payload: {
+          type: "bad",
+          title: "Пост не створено",
+          text: "Мінімум 8 літер в назві, і 100 в контенті."
+        }
+      });
 
     fetch("/api/posts", {
       method: "POST",
@@ -71,10 +99,27 @@ export default function CreatePost() {
       .then((res) => res.json())
       .then((res) => {
         if (res.status === "success") {
+          dispatch({
+            type: "addNotification",
+            payload: {
+              type: "good",
+              title: "Пост створено",
+              text: "Відбулося перенаправлення."
+            }
+          });
           router.replace("/posts/" + res.data.id);
+        } else {
+          dispatch({
+            type: "addNotification",
+            payload: {
+              type: "bad",
+              title: "Пост не створено",
+              text: "Вкажіть теги. Якщо не допомогло, проблема на сервері ;("
+            }
+          });
         }
       })
-      .catch(console.log);
+      .catch(console.error);
 
     // if(files !== undefined && files.length > 1) {
     //     const form = new FormData();
